255c1674ea2fc45929745c9552f696a079a7e710
Andrea Ferloni <registrationferlo@yahoo.it>
2022-05-05 15:20:24 +0200

    Changed the code to include also links that are 0, to be able to perform QAP. Instead of creating the network and then extracting edgelist, I do that with a new formula (see line 155).

--- 1-co_occurrence_regpat.R
+++ 1-co_occurrence_regpat.R
@@ -7,10 +7,12 @@ CPC<- fread("/Users/aferloni/switchdrive/Andrea-Celine/These Andrea/Paper 2 - Ur
 nuts<- read.table("/Users/aferloni/switchdrive/Andrea-Celine/These Andrea/Paper 2 - Urban relatedness/DATA/nuts_patents.txt.zip", header= TRUE, sep= '|', stringsAsFactors = FALSE)
 no_nuts<- read.table("/Users/aferloni/switchdrive/Andrea-Celine/These Andrea/Paper 2 - Urban relatedness/DATA/no_nuts.txt.zip", header= TRUE, sep= '|', stringsAsFactors = FALSE)
 
-#nuts<- read.table("/Users/aferloni/Documents/Geographic data/nuts_patents.txt", header= TRUE, sep= '|', stringsAsFactors = FALSE)
-#no_nuts<- read.table("/Users/aferloni/Documents/Geographic data/NO-NUTS/no_nuts.txt", header= TRUE, sep= '|', stringsAsFactors = FALSE)
+nuts<- read.table("/Users/aferloni/Documents/Geographic data/nuts_patents.txt", header= TRUE, sep= '|', stringsAsFactors = FALSE)
+no_nuts<- read.table("/Users/aferloni/Documents/Geographic data/NO-NUTS/no_nuts.txt", header= TRUE, sep= '|', stringsAsFactors = FALSE)
 
 all_patents<- rbind(nuts, no_nuts)
+
+CPC$CPC_Class<- substr(CPC$CPC_Class, 1, 4)
 
 #we should now select the year so that we create four different periods
 
@@ -64,7 +66,7 @@ patents_4<- patents_4%>%
 #this is the starting point, let's now continue by building a matrix
 ##-PERIOD 1
 
-Co_occurrence_1<- Matrix::crossprod(xtabs(~ appln_id + group_id, data=patents_1, sparse=TRUE))
+Co_occurrence_1<- Matrix::crossprod(xtabs(~ appln_id + group_id, data=patents_1, sparse=F))
 
 ###normalize matrix so that all values are comprised between 0 and 1
 
@@ -76,7 +78,7 @@ M2<- Co_occurrence_1/sqrt(M1)
 diag(M2)<- 1
 
 Co_occurrence_1_mat<- as.matrix(M2) #first transform to regular matrix
-#Co_occurrence_1_DF<- as.data.frame(Co_occurrence_1_mat)# if necessary, back to DF
+Co_occurrence_1_DF<- as.data.frame(Co_occurrence_1_mat)# then, back to DF
 
 ##-PERIOD 2
 
@@ -92,7 +94,7 @@ M2<- Co_occurrence_2/sqrt(M1)
 diag(M2)<- 1
 
 Co_occurrence_2_mat<- as.matrix(M2) #first transform to regular matrix
-#Co_occurrence_2_DF<- as.data.frame(Co_occurrence_2_mat)# if necessary, back to DF
+Co_occurrence_2_DF<- as.data.frame(Co_occurrence_2_mat)# then, back to DF
 
 ##-PERIOD 3
 
@@ -108,7 +110,7 @@ M2<- Co_occurrence_3/sqrt(M1)
 diag(M2)<- 1
 
 Co_occurrence_3_mat<- as.matrix(M2) #first transform to regular matrix
-#Co_occurrence_3_DF<- as.data.frame(Co_occurrence_3_mat)# if necessary, back to DF
+Co_occurrence_3_DF<- as.data.frame(Co_occurrence_3_mat)# then, back to DF
 
 ##-PERIOD 4
 
@@ -124,56 +126,82 @@ M2<- Co_occurrence_4/sqrt(M1)
 diag(M2)<- 1
 
 Co_occurrence_4_mat<- as.matrix(M2) #first transform to regular matrix
-#Co_occurrence_4_DF<- as.data.frame(Co_occurrence_4_mat)# if necessary, back to DF
+Co_occurrence_4_DF<- as.data.frame(Co_occurrence_4_mat)# then, back to DF
+
+#check which codes are not in previous periods. Also obtain node list
+
+names_4<- as.data.frame(row.names(Co_occurrence_4_DF))%>%
+  rename(code=1)
+names_3<- as.data.frame(row.names(Co_occurrence_3_DF))%>%
+  rename(code=1)
+names_2<- as.data.frame(row.names(Co_occurrence_2_DF))%>%
+  rename(code=1)
+names_1<- as.data.frame(row.names(Co_occurrence_1_DF))%>%
+  rename(code=1)
+
+setdiff(names_4, names_3)# G99Z codes in 4 that are not in 3
+setdiff(names_3, names_4)# "B41B" "F21H" "G21Y" codes in 3 that are not in 4
+setdiff(names_3, names_2)# "F24J" "G06D" "G16Y" "G21J" "G21Y" "H04T" codes in 3 that are not in 2
+setdiff(names_2, names_3)# 0 all codes in 2 are in 3
+setdiff(names_2, names_1)# 0 all codes in 2 are in 1
+setdiff(names_1, names_2)# "G21J" code in 1 that is not in 2
+
 
 #---now let's transform each period into edgelist to generate the network
 ##en passant, remove 1 values (duplicated links) because network is undirected (AB=BA). I thought the weights = 0 had to be kept but in reality there's none..
 
-g1<- graph.adjacency(Co_occurrence_1_mat, weighted = T)
-period1_edgelist<- get.data.frame(g1, what = "edges")
+#g1<- graph.adjacency(Co_occurrence_1_mat, mode="upper", weighted = T, diag=F) #not useful anymore because it doesn't extract the links with weight 0.
+
+period1_edgelist<- data.frame(source= rownames(Co_occurrence_1_DF)[col(Co_occurrence_1_DF)], target=colnames(Co_occurrence_1_DF)[row(Co_occurrence_1_DF)], weight= c(t(Co_occurrence_1_DF)))
+
+#this seems to work without the need to transform it to network and take edgelist. I HAVE NO IDEA WHY; BUT IT WORKS.
+
+#period1_edgelist<- get.data.frame(g1, what = "edges")
+
 
 period1_edgelist<- period1_edgelist%>%
   filter(weight != 1)
 
-links_1<- period1_edgelist[!duplicated(t(apply(period1_edgelist[c("from", "to")], 1, sort))), ]
+#links_1<- period1_edgelist[!duplicated(t(apply(period1_edgelist[c("from", "to")], 1, sort))), ] old version
+links_1<- period1_edgelist[!duplicated(t(apply(period1_edgelist[c("source", "target")], 1, sort))), ] #this works, we get all links including those that are 0. 
+
 
 #2
-g2<- graph.adjacency(Co_occurrence_2_mat, weighted = T)
-period2_edgelist<- get.data.frame(g2, what = "edges")
+#g2<- graph.adjacency(Co_occurrence_2_mat, weighted = T)
+#period2_edgelist<- get.data.frame(g2, what = "edges")
+
+period2_edgelist<- data.frame(source= rownames(Co_occurrence_2_DF)[col(Co_occurrence_2_DF)], target=colnames(Co_occurrence_2_DF)[row(Co_occurrence_2_DF)], weight= c(t(Co_occurrence_2_DF)))
 
 period2_edgelist<- period2_edgelist%>%
   filter(weight != 1)
 
-links_2<- period2_edgelist[!duplicated(t(apply(period2_edgelist[c("from", "to")], 1, sort))), ]
+links_2<- period2_edgelist[!duplicated(t(apply(period2_edgelist[c("source", "target")], 1, sort))), ]
 
 #3
 
-g3<- graph.adjacency(Co_occurrence_3_mat, weighted = T)
-period3_edgelist<- get.data.frame(g3, what = "edges")
+#g3<- graph.adjacency(Co_occurrence_3_mat, weighted = T)
+#period3_edgelist<- get.data.frame(g3, what = "edges")
+
+period3_edgelist<- data.frame(source= rownames(Co_occurrence_3_DF)[col(Co_occurrence_3_DF)], target=colnames(Co_occurrence_3_DF)[row(Co_occurrence_3_DF)], weight= c(t(Co_occurrence_3_DF)))
 
 period3_edgelist<- period3_edgelist%>%
   filter(weight != 1)
 
-links_3<- period3_edgelist[!duplicated(t(apply(period3_edgelist[c("from", "to")], 1, sort))), ]
+links_3<- period3_edgelist[!duplicated(t(apply(period3_edgelist[c("source", "target")], 1, sort))), ]
+
 #4
 
-g4<- graph.adjacency(Co_occurrence_4_mat, weighted = T)
-period4_edgelist<- get.data.frame(g4, what = "edges")
+#g4<- graph.adjacency(Co_occurrence_4_mat, weighted = T)
+#period4_edgelist<- get.data.frame(g4, what = "edges")
+
+period4_edgelist<- data.frame(source= rownames(Co_occurrence_4_DF)[col(Co_occurrence_4_DF)], target=colnames(Co_occurrence_4_DF)[row(Co_occurrence_4_DF)], weight= c(t(Co_occurrence_4_DF)))
 
 period4_edgelist<- period4_edgelist%>%
   filter(weight != 1) 
 
-links_4<- period4_edgelist[!duplicated(t(apply(period4_edgelist[c("from", "to")], 1, sort))), ]
-
-
-#extract also nodes and calculate the relative frequency of each code to map node size
-
-nodes_1<- get.data.frame(g1, what = "vertices")
-nodes_2<- get.data.frame(g2, what = "vertices")
-nodes_3<- get.data.frame(g3, what = "vertices")
-nodes_4<- get.data.frame(g4, what = "vertices")
+links_4<- period4_edgelist[!duplicated(t(apply(period4_edgelist[c("source", "target")], 1, sort))), ]
 
 # write file to export to network
 write.csv(links_4, file= "/Users/aferloni/Documents/Thesis-second paper/2_methods/co_occurrence/OECD-regpat/period4_links.csv", quote =FALSE, row.names = FALSE)
-write.csv(nodes_4, file= "/Users/aferloni/Documents/Thesis-second paper/2_methods/co_occurrence/OECD-regpat/period4_nodes.csv", quote =FALSE, row.names = FALSE)
+write.csv(names_4, file= "/Users/aferloni/Documents/Thesis-second paper/2_methods/co_occurrence/OECD-regpat/period4_nodes.csv", quote =FALSE, row.names = FALSE)
 
